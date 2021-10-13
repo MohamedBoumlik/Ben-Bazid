200e67133b551f1dff02d0ece27dfa4ace304fd3
MohamedBoumlik <mohamed.boumlik.19@gmail.com>
2021-10-13 21:55:10 +0100

    modifications

--- src/page/Products.js
+++ src/page/Products.js
@@ -26,12 +26,6 @@ function Products() {
         setShowModal(!modal);
     };
 
-    if(modal) {
-        document.body.classList.add('active-modal')
-    } else {
-        document.body.classList.remove('active-modal')
-    }
-
     return (
         <div>
 
