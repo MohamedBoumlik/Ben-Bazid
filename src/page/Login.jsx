db4140d716569154afceb0bbf7d34e3721d95feb
MohamedBoumlik <mohamed.boumlik.19@gmail.com>
2021-10-23 17:50:09 +0100

    login not posting

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,12 +1,15 @@
 import React ,{useState} from 'react';
 import { axios } from 'axios';
-
+import { useHistory } from 'react-router';
+import swal from 'sweetalert';
 function Login() {
 
+    const history = useHistory();
 
     const [loginInput, setLogin] = useState({
         email: '',
         password: '',
+        error_list: [],
     })
 
     const handleInput = (e) => {
@@ -21,11 +24,33 @@ function Login() {
             email: loginInput.email,
             password: loginInput.password,
         }
+        
+        console.log(data)
 
-        axios.post(`api/login`, data)
-        .then(res =>{
+        // axios.post(`/api/login`, data)
+        // .then(res =>{
+        //         console.log(res)
+        //     // if(res.data.status === 200){
 
-        });
+        //     //     // localStorage.setItem('auth_token', res.data.token);
+        //     //     // localStorage.setItem('name', res.data.name);
+        //     //     // swal('Success',res.data.message,'success');
+        //     //     // history.push('/admin/produits')
+        //     //     console.log(res)
+
+        //     // }else if(res.data.status === 401){
+
+        //     //     // swal('Warning',res.data.message,'warning')
+        //     //     console.log(res)
+
+
+        //     // }else{
+        //     //     setLogin({...loginInput, error_list: res.data.validation_errors})
+
+        //     // }
+
+        // });
+
     }
 
 
@@ -45,17 +70,20 @@ function Login() {
                         <div className="card-body">
 
                             <form onSubmit={loginSubmit}>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleInput} name='email' value={loginInput.email} />
-                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                                <div className="mb-3">
+                                    <label forhtml="exampleInputEmail1" className="form-label">Email address</label>
+                                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={handleInput} name='email' value={loginInput.email} />
+                                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                    <span>{loginInput.error_list.email}</span>
                                 </div>
-                                <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" onChange={handleInput} name='password' value={loginInput.password}/>
+
+                                <div className="mb-3">
+                                    <label forhtml="exampleInputPassword1" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="exampleInputPassword1" onChange={handleInput} name='password' value={loginInput.password}/>
+                                    <span>{loginInput.error_list.password}</span>
                                 </div>
                                 <div className="text-center">
-                                    <button type="submit" class="btn btn-outline-dark">Login</button>
+                                    <button type="submit" className="btn btn-outline-dark">Login</button>
                                 </div>
                             </form>
 
